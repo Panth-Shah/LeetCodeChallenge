d8afb2a6db3a9fd21dcfbbe10bd2219c76899b37
Panth Shah <panth5shah@gmail.com>
2021-01-05 17:03:08 -0500

    [JanuaryChallenge] - Day 5 Remove Duplicate from Sorted List solution

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day5-RemoveDuplicateFromSortedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day5_RemoveDuplicateFromSortedList
+    {
+        public ListNode DeleteDuplicateNode(ListNode head)
+        {
+            //sentinel
+            ListNode sentinel = new ListNode(0, head);
+            ListNode pred = sentinel;
+            while (head != null)
+            {
+                if (head.next != null && head.val == head.next.val)
+                {
+                    //move till the end of duplicate sublist
+                    while (head.next != null && head.val == head.next.val)
+                    {
+                        head = head.next;
+                    }
+                    //skip all the duplicate nodes
+                    pred.next = head.next;
+                }
+                else
+                {
+                    //keep pred moving forward
+                    pred = pred.next;
+                }
+                head = head.next;
+            }
+
+            return sentinel.next;
+        }
+    }
+}
