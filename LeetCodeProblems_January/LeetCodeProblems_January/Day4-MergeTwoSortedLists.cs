84517a548d066f78ef184ef1f93e5a779620b34b
Panth Shah <panth5shah@gmail.com>
2021-01-04 22:53:19 -0500

    [JanuaryChallenge] - Day 4  Merge Two Sorted Lists solution commit

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day4-MergeTwoSortedLists.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day4_MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode preHead = new ListNode(-1);
+            ListNode prev = preHead;
+            while (l1 != null && l2!= null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+            //either l1 or l2 can be non -null at this point
+            prev.next = l1 == null ? l2 : l1;
+            return preHead.next;
+        }
+    }
+
+    
+     // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
