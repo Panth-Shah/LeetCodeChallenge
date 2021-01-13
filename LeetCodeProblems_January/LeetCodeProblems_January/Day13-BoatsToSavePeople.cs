c2c1260f294cd9257493c7c7581ef5a45340d031
Panth Shah <panth5shah@gmail.com>
2021-01-13 18:02:58 -0500

    [January Challenge] - Boat to Save People problem solution updated

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day13-BoatsToSavePeople.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day13_BoatsToSavePeople
+    {
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            int p1 = 0, p2 = people.Length - 1, ans = 0;
+            while (p1 <= p2)
+            {
+                if (people[p1] + people[p2] <= limit)
+                {
+                    p1 += 1;
+                }
+                ans += 1;
+                p2 -= 1;
+            }
+            return ans;
+        }
+    }
+}
