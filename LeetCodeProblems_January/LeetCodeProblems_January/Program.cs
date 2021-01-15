6c8343159fae37cec575a040be79f2827ae46284
Panth-Shah <panth5shah@gmail.com>
2021-01-14 21:56:24 -0500

    [January Challenge] - Reduce Variable to 0 Sliding Window solution updated

--- LeetCodeProblems_January/LeetCodeProblems_January/Program.cs
+++ LeetCodeProblems_January/LeetCodeProblems_January/Program.cs
@@ -10,8 +10,11 @@ namespace LeetCodeProblems_January
     {
         public static void Main(string[] args)
         {
-            Day11_MergeSortedList d11 = new Day11_MergeSortedList();
-            d11.Merge(new int[] {1,2,4,5,6,0}, 5, new int[] {3}, 1);
+            //    Day11_MergeSortedList d11 = new Day11_MergeSortedList();
+            //    d11.Merge(new int[] {1,2,4,5,6,0}, 5, new int[] {3}, 1);
+
+            Day14_ReduceVariableTo0 d14 = new Day14_ReduceVariableTo0();
+            d14.MinOperations(new int[] { 3,2,20,1,1,3}, 10);
 
         }
     }
