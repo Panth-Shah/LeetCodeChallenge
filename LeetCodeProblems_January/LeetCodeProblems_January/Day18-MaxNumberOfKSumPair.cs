ae25c84759b9b4fdd21301faba6a53664c8bc52e
Panth-Shah <panth5shah@gmail.com>
2021-01-19 01:34:42 -0500

    [January Challenge] - Max Number of K-Sum Pair solution updated

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day18-MaxNumberOfKSumPair.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day18_MaxNumberOfKSumPair
+    {
+        public int MaxOperations(int[] nums, int k)
+        {
+            int result = 0;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            foreach (int ele in nums)
+            {
+                if (map.ContainsKey(k - ele))
+                {
+                    int freq = map[k - ele];
+                    if (freq == 1)
+                    {
+                        map.Remove(k - ele);
+                    }
+                    else
+                    {
+                        map[k - ele] = freq - 1;
+                    }
+                    result++;
+                }
+                else
+                {
+                    if (map.ContainsKey(ele))
+                    {
+                        map[ele] += 1;
+                    }
+                    else
+                    {
+                        map.Add(ele, 1);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
