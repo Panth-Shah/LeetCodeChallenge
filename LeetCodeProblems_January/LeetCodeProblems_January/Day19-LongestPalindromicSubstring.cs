b5db554db98021f639fe8616faf9f795e3c81a85
Panth-Shah <panth5shah@gmail.com>
2021-01-22 23:00:49 -0500

    [January Challenge] - Longest Palindromic Substring and Most Competitive Subsequence solutions updated

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day19-LongestPalindromicSubstring.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day19_LongestPalindromicSubstring
+    {
+        public string longestPalindrome(string s)
+        {
+            int start = 0, end = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int odd = expand(s, i, i);
+                int even = expand(s, i, i + 1);
+                int len = Math.Max(odd, even);
+                if (len > end - start)
+                {
+                    //Even len (6)-> 2 start --> i-2, end -> i+3
+                    //Odd len (5) -> 2 start i-2, end -> i+2
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+            return s.Substring(start, end + 1);
+        }
+        int expand(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+            return right - left - 1;
+        }
+    }
+}
---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day21_MostCompetitiveSubsequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day21_MostCompetitiveSubsequence
+    {
+        public int[] MostCompetitive(int[] nums, int k)
+        {
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (stack.Count > 0)
+                {
+                    while (stack.Count + (nums.Length - i) > k && stack.Count > 0 && stack.Peek() > nums[i])
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count < k)
+                    {
+                        stack.Push(nums[i]);
+                    }
+                }
+                else
+                {
+                    stack.Push(nums[i]);
+                }
+            }
+
+            return stack.Reverse().ToArray();
+        }
+}
+}
