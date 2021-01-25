1c1e81224a0a86237da8010ace54a0c437e9744a
Panth-Shah <panth5shah@gmail.com>
2021-01-25 00:39:51 -0500

    [January Challenge] - Day 23 & 24 solution updated

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day23_SortMatricDiagonally.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day23_SortMatricDiagonally
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            int rowCount = mat.Length;
+            int columnCount = mat[0].Length;
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    int diagonalKey = i - j;
+                    if (!map.ContainsKey(diagonalKey))
+                    {
+                        map.Add(diagonalKey, new List<int>());
+                    }
+                    map[diagonalKey].Add(mat[i][j]);
+                }
+            }
+
+            foreach (var ele in map)
+            {
+                ele.Value.Sort();
+            }
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    int diagonalKey = i - j;
+                    mat[i][j] = map[i - j][0];
+                    map[i - j].RemoveAt(0);
+                }
+            }
+
+            return mat;
+        }
+    }
+}
---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day24_MergeKSortedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day24_MergeKSortedList
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists == null || lists.Length == 0) return null;
+
+            int interval = 1;
+            while (interval < lists.Length)
+            {
+                for (int i = 0; i + interval < lists.Length; i = i + interval * 2)
+                {
+                    lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+                }
+                interval *= 2;
+            }
+            return lists[0];
+        }
+
+        ListNode mergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            ListNode newHead = new ListNode(l1.val);
+            ListNode temp = newHead;
+            while (l2 != null && l1 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    temp.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                }
+                else
+                {
+                    temp.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                }
+                temp = temp.next;
+            }
+
+            if (l1 != null)
+            {
+                temp.next = l1;
+            }
+            else
+            {
+                temp.next = l2;
+            }
+            return newHead.next;
+        }
+    }
+}
