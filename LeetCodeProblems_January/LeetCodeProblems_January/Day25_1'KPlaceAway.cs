63ebe1a6ea5b5848c8123ce39426baa04e613f7b
Panth-Shah <panth5shah@gmail.com>
2021-01-25 23:17:14 -0500

    [January Challenge] - Day 25 solution updated

---
+++ LeetCodeProblems_January/LeetCodeProblems_January/Day25_1'KPlaceAway.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_January
+{
+    public class Day25_1_KPlaceAway
+    {
+        public bool KLengthApart(int[] nums, int k)
+        {
+            int count = k;
+            foreach (int num in nums)
+            {
+                if (num == 1)
+                {
+                    if (count < k)
+                    {
+                        return false;
+                    }
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return true;
+        }
+    }
+}
