6221af02f20d6a1c389713d31b8793e38b4b9b74
Panth-Shah <panth5shah@gmail.com>
2021-03-02 21:08:44 -0500

    [March Challenge] - Set Mismatch problem solution

---
+++ LeetCodeProblems_March/Day2_SetMismatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    //Time: O(n)
+    //Space: O(1)
+    public class Day2_SetMismatch
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int len = nums.Length;
+            int dup = -1, missing = 1;
+            foreach (int num in nums)
+            {
+                if (nums[Math.Abs(num) - 1] < 1)
+                {
+                    dup = Math.Abs(num);
+                }
+                else
+                {
+                    nums[Math.Abs(num) - 1] *= -1;
+                }
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                    missing = i + 1;
+            }
+            return new int[] { dup, missing };
+        }
+    }
+}
