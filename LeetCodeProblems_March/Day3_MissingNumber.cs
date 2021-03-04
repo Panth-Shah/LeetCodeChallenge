13cd0f03aefad317392bf2b6b5b160499d65633e
Panth-Shah <panth5shah@gmail.com>
2021-03-03 23:13:51 -0500

    [March Challenge] - Day 3 Missing Number solution updated

---
+++ LeetCodeProblems_March/Day3_MissingNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    public class Day3_MissingNumber
+    {
+        //Time: O(N)
+        //Space: O(N)
+        public int MissingNumber(int[] nums)
+        {
+            int len = nums.Length;
+            int[] arr = new int[len + 1];
+            int result = -1;
+            foreach (int num in nums)
+            {
+                arr[num] = 1;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    result = i;
+                }
+            }
+            return result;
+        }
+    }
+}
