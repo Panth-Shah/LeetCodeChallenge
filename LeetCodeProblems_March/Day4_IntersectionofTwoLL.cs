9f4834f3d0ace344e6715bd97808bb7a1aaa772f
Panth-Shah <panth5shah@gmail.com>
2021-03-05 09:17:11 -0500

    [March Challenge] - Identify intersection of two linked lists solution updated

---
+++ LeetCodeProblems_March/Day4_IntersectionofTwoLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    public class Day4_IntersectionofTwoLL
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            ListNode currA = headA;
+            ListNode currB = headB;
+
+            while (currA != currB)
+            {
+                currA = currA != null ? currA.next : headB;
+                currB = currB != null ? currB.next : headA;
+            }
+            return currA;
+
+        }
+    }
+    //Definition for singly-linked list. 
+    public class ListNode
+    {
+        public int val;     
+        public ListNode next;    
+        public ListNode(int x) { val = x; }
+    }
+}
