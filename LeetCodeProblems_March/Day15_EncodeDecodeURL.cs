918701222a2e87453df50c766916cec58eecd902
Panth-Shah <panth5shah@gmail.com>
2021-03-15 21:45:15 -0400

    [March Solution] - Encode and Decode TinyURL solution uploaded

---
+++ LeetCodeProblems_March/Day15_EncodeDecodeURL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    public class Day15_EncodeDecodeURL
+    {
+        //535. Encode and Decode TinyURL
+        public class Codec
+        {
+            Dictionary<int, string> dic;
+            const string baseURL = "http://tinyurl.com/";
+
+            public Codec()
+            {
+                this.dic = new Dictionary<int, string>();
+            }
+            // Encodes a URL to a shortened URL
+            public string encode(string longUrl)
+            {
+                var key = longUrl.GetHashCode();
+                dic.Add(key, longUrl);
+                return baseURL + key;
+            }
+
+            // Decodes a shortened URL to its original URL.
+            public string decode(string shortUrl)
+            {
+                var url = shortUrl.Replace(baseURL, "");
+                var urlKey = int.Parse(url);
+                return dic[urlKey];
+            }
+        }
+    }
+}
