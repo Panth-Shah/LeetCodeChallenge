fa36c9de999fde358b6bce64a683575533ed9a60
Panth-Shah <panth5shah@gmail.com>
2021-03-17 23:23:24 -0400

    [March Challenge] - Buy Sell Stock with max profit solution uploaded

---
+++ LeetCodeProblems_March/Day16_BuySellStocks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    public class Day16_BuySellStocks
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            int cash = 0, hold = -prices[0];
+            for (int i = 1; i < prices.Length; i++)
+            {
+                cash = Math.Max(cash, hold + prices[i] - fee);
+                hold = Math.Max(hold, cash - prices[i]);
+            }
+            return cash;
+        }
+    }
+}
