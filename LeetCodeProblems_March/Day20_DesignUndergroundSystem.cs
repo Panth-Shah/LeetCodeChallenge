1fa49517c7e2af30d28b22f5e33b26c1f71532ac
Panth-Shah <panth5shah@gmail.com>
2021-03-20 23:54:31 -0400

    [March Challenge] - Design Underground System solution class added

---
+++ LeetCodeProblems_March/Day20_DesignUndergroundSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems_March
+{
+    public class Day20_DesignUndergroundSystem
+    {
+        public Day20_DesignUndergroundSystem()
+        {
+
+        }
+
+        public void CheckIn(int id, string stationName, int t)
+        {
+
+        }
+
+        public void CheckOut(int id, string stationName, int t)
+        {
+
+        }
+
+        public double GetAverageTime(string startStation, string endStation)
+        {
+            double res = 0;
+            return res;
+        }
+    }
+
+    /**
+     * Your UndergroundSystem object will be instantiated and called as such:
+     * UndergroundSystem obj = new UndergroundSystem();
+     * obj.CheckIn(id,stationName,t);
+     * obj.CheckOut(id,stationName,t);
+     * double param_3 = obj.GetAverageTime(startStation,endStation);
+     */
+}
